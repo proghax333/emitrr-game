70f2feb16bf643cbd1626ffc7a68c412cff68db2
proghax333 <skillzworldtech@gmail.com>
2024-02-15 03:57:29 +0530

    fix: env loader

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	PORT := os.Getenv("PORT")
